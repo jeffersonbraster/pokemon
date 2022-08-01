95dbce6e504c53a3bc495e0d056e5ad75e3c3ad5
Jefferson <jeferson.oliveira@g.globo>
2022-08-01 19:34:49 -0300

    header responsivo

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -3,23 +3,22 @@ var slide_hero = new Swiper(".slide-hero", {
   effect: "fade",
   pagination: {
     el: ".slide-hero .main-area .area-explore .swiper-pagination ",
-  }
-})
+  },
+});
 
-
-const cardPokemon = document.querySelectorAll('.js-open-details-pokemon');
-const btnCloseModal = document.querySelector('.js-close-modal-details-pokemon');
+const cardPokemon = document.querySelectorAll(".js-open-details-pokemon");
+const btnCloseModal = document.querySelector(".js-close-modal-details-pokemon");
 
 function openDetailsPokemon() {
-  document.documentElement.classList.add('open-modal');
+  document.documentElement.classList.add("open-modal");
 }
 
-cardPokemon.forEach(card => {
-  card.addEventListener('click', openDetailsPokemon);
-})
+cardPokemon.forEach((card) => {
+  card.addEventListener("click", openDetailsPokemon);
+});
 
 function closeDetailsPokemon() {
-  document.documentElement.classList.remove('open-modal');
+  document.documentElement.classList.remove("open-modal");
 }
 
-btnCloseModal.addEventListener('click', closeDetailsPokemon);
+btnCloseModal.addEventListener("click", closeDetailsPokemon);
