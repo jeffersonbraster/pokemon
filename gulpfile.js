3fd3ebf7a807b1fdb1b133f70df0d95528afd0f5
Jefferson <jeferson.oliveira@g.globo>
2022-08-02 21:54:43 -0300

    listando pokemons da api

--- gulpfile.js
+++ gulpfile.js
@@ -54,7 +54,7 @@ gulp.task("allJs", gulpJs);
 
 function pluginsJs() {
   return gulp
-    .src(["./js/lib/swiper.min.js"])
+    .src(["./js/lib/swiper.min.js", "./js/lib/axios.min.js"])
     .pipe(concat("plugins.js"))
     .pipe(gulp.dest("js/"))
     .pipe(browserSync.stream());
--- js/scripts/main.js
+++ js/scripts/main.js
@@ -9,17 +9,9 @@ var slide_hero = new Swiper(".slide-hero", {
 const cardPokemon = document.querySelectorAll(".js-open-details-pokemon");
 const btnCloseModal = document.querySelector(".js-close-modal-details-pokemon");
 
-function openDetailsPokemon() {
-  document.documentElement.classList.add("open-modal");
-}
-
 cardPokemon.forEach((card) => {
   card.addEventListener("click", openDetailsPokemon);
 });
-
-function closeDetailsPokemon() {
-  document.documentElement.classList.remove("open-modal");
-}
 
 if (btnCloseModal) {
   btnCloseModal.addEventListener("click", closeDetailsPokemon);
@@ -30,3 +22,104 @@ const btnDropDownSelect = document.querySelector(".js-open-select-custom");
 btnDropDownSelect.addEventListener("click", () => {
   btnDropDownSelect.parentElement.classList.toggle("active");
 });
+
+const areaPokemons = document.getElementById("js-list-pokemons");
+
+function primeiraLetraMaiuscula(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function createCardPokemon(code, type, nome, imagePok) {
+  let card = document.createElement("button");
+  card.classList = `card-pokemon js-open-details-pokemon ${type}`;
+  areaPokemons.appendChild(card);
+
+  let image = document.createElement("div");
+  image.classList = "image";
+  card.appendChild(image);
+
+  let imageSrc = document.createElement("img");
+  imageSrc.classList = "thumb-img";
+  imageSrc.setAttribute("src", imagePok);
+  image.appendChild(imageSrc);
+
+  let infoCardPokemon = document.createElement("div");
+  infoCardPokemon.classList = "info";
+  card.appendChild(infoCardPokemon);
+
+  let infoTextPokemon = document.createElement("div");
+  infoTextPokemon.classList = "text";
+  infoCardPokemon.appendChild(infoTextPokemon);
+
+  let codePokemon = document.createElement("span");
+  codePokemon.textContent =
+    code < 10 ? `#00${code}` : code < 100 ? `#0${code}` : `#${code}`;
+  infoTextPokemon.appendChild(codePokemon);
+
+  let namePokemon = document.createElement("h3");
+  namePokemon.textContent = primeiraLetraMaiuscula(nome);
+  infoTextPokemon.appendChild(namePokemon);
+
+  let areaIcon = document.createElement("div");
+  areaIcon.classList = "icon";
+  infoCardPokemon.appendChild(areaIcon);
+
+  let imgType = document.createElement("img");
+  imgType.setAttribute("src", `img/icon-types/${type}.svg`);
+  areaIcon.appendChild(imgType);
+}
+
+function listingPokemons(url) {
+  axios({
+    method: "GET",
+    url,
+  }).then((response) => {
+    const countPokemons = document.getElementById("js-count-pokemons");
+    const { results, count } = response.data;
+
+    countPokemons.innerText = count;
+
+    results.forEach((pokemon) => {
+      let urlApiDetails = pokemon.url;
+
+      axios({
+        method: "GET",
+        url: `${urlApiDetails}`,
+      }).then((response) => {
+        const { id, name, sprites, types } = response.data;
+
+        const infoCard = {
+          nome: name,
+          code: id,
+          image: sprites.other.dream_world.front_default,
+          type: types[0].type.name,
+        };
+
+        createCardPokemon(
+          infoCard.code,
+          infoCard.type,
+          infoCard.nome,
+          infoCard.image
+        );
+
+        const cardPokemon = document.querySelectorAll(
+          ".js-open-details-pokemon"
+        );
+
+        cardPokemon.forEach((card) => {
+          card.addEventListener("click", openDetailsPokemon);
+        });
+      });
+    });
+  });
+}
+
+listingPokemons("https://pokeapi.co/api/v2/pokemon?limit=9&offset=0");
+
+function openDetailsPokemon() {
+  document.documentElement.classList.add("open-modal");
+}
+
+function closeDetailsPokemon() {
+  document.documentElement.classList.remove("open-modal");
+}
